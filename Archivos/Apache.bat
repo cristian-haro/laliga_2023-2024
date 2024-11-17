52a4efbaf3abf2deba64d9bf15f667248be96bba
Cristian Haro <cristianharoalvarez@gmail.com>
2024-11-17 16:29:47 +0100

    Remove

--- Archivos/Apache.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-cls
-
-tasklist | findstr /i "httpd.exe"
-if "!errorlevel!" NEQ "1" (
-    echo Activo
-)else C:\xampp\apache_start.bat
--- Archivos/MySQL.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-cls
-
-tasklist | findstr /i "mysqld.exe"
-if "!errorlevel!" NEQ "1" (
-    echo Activo
-)else C:\xampp\mysql_start.bat
